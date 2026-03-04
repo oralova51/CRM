00fc5b747d012b408b4f930badb1515d5a9aa050
oralova51 <oralova51@gmail.com>
2026-03-04 14:58:58 +0200

    модель, миграция, сиды LoyalityLevel созданы

---
+++ server/db/migrations/20260304124544-create-loyalty-level.js
@@ -0,0 +1,39 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("LoyaltyLevels", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      min_spent: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      discount_pct: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("LoyaltyLevels");
+  },
+};
