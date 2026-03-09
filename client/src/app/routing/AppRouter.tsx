25de8e637dd246ab6078abb1786539c7833d2a99
drJanebug <rainbowbug98@gmail.com>
2026-03-09 17:33:38 +0500

    fixing conflicts

--- client/src/app/routing/AppRouter.tsx
+++ client/src/app/routing/AppRouter.tsx
@@ -2,7 +2,8 @@ import { Routes, Route } from "react-router";
 import Layout from "../Layout/Layout";
 import AuthPage from "../../pages/AuthPage/AuthPage";
 import MainPage from "../../pages/MainPage/MainPage";
-import TasksPage from "../../pages/TasksPage/TasksPage";
+import ProcedureCalendarPage from "../../pages/ProcedureCalendarPage/ProcedureCalendarPage";
+import BookAppointmentPage from "../../pages/BookAppointmentPage/BookAppointmentPage";
 import { CLIENT_ROUTES } from "../../shared/consts/clientRoutes";
 
 export default function AppRouter() {
@@ -17,7 +18,7 @@ export default function AppRouter() {
         />
         <Route
           path={CLIENT_ROUTES.BOOK.slice(1)}
-          element={<BookAppointmentPage />}
+          element={<BookAppointmentPage />} 
         />
         <Route path="*" element={<h1>Нет контента</h1>} />
         {/* <Route path={CLIENT_ROUTES.TASKS.slice(1)} element={<TasksPage />} /> */}
